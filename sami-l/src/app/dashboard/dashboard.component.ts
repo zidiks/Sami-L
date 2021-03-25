29338cc55ce12c1172a0ffc1a5e37715f8b43abf
Mironov Vladimir <zidiks228@gmail.com>
2021-03-25 04:09:52 +0300

    feat: add menu

--- sami-l/src/app/dashboard/dashboard.component.ts
+++ sami-l/src/app/dashboard/dashboard.component.ts
@@ -19,9 +19,121 @@ export class DashboardComponent implements OnInit {
     weeklyIncome: 9000
   }
 
+  activeCategory = 0;
+
+  menu = [
+    {
+      id: 0,
+      name: 'Pizza',
+      icon: 'icon-pizza',
+      items: [
+        {
+          name: 'Italian Pizza',
+          price: 35,
+          img: 'pizza5.jpg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: '365 Pizza',
+          price: 35,
+          img: 'pizza4.jpg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: 'Maxican Pizza',
+          price: 35,
+          img: 'pizza3.jpg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: 'New York Pizza',
+          price: 35,
+          img: 'pizza.jpg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: 'Louis Pizza',
+          price: 35,
+          img: 'pizza6.jpg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: 'California Pizza',
+          price: 35,
+          img: 'pizza7.jpg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: 'Deluxe Pizza',
+          price: 35,
+          img: 'pizza2.jpg',
+          orders: 40,
+          revenue: 1200
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: 'Burger',
+      icon: 'icon-burger',
+      items: [
+        {
+          name: 'Big Burger',
+          price: 35,
+          img: 'burger.jpeg',
+          orders: 40,
+          revenue: 1200
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Pasta',
+      icon: 'icon-icecream',
+      items: [
+        {
+          name: 'Italian Pasta',
+          price: 35,
+          img: 'pasta.jpeg',
+          orders: 40,
+          revenue: 1200
+        },
+        {
+          name: 'Masala Pasta',
+          price: 35,
+          img: 'pasta2.jpeg',
+          orders: 40,
+          revenue: 1200
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Cake',
+      icon: 'icon-cake',
+      items: [
+        
+      ]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isSelectedItem(id: number) {
+    return id == this.activeCategory;
+  }
+
+  selectItem(id: number) {
+    this.activeCategory = id;
   }
 
 }
